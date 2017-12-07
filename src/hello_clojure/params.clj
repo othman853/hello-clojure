e6b5fc11e5d19db0cea33bd8f76bbc61e45fd10e
othman853 <anuar.yasser@gmail.com>
2017-12-07 20:47:58 -0200

    functions returning functions

--- src/hello_clojure/params.clj
+++ src/hello_clojure/params.clj
@@ -1,9 +1,14 @@
 (ns hello-clojure.params
   (:require [clojure.string :refer [split]]))
 
+(defn separate
+  [token]
+  #(split % token))
+
 (defn parse
   ([url]
     (if (= (count url) 0)
       []
-      (map (fn [param] (split param #"=")) (split url #"&"))))
+      (map (separate #"=")
+           ((separate #"&") url))))
   ([] []))
