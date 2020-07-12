55608b7dccb347dd18c2c25fabfc28ed4d35d4e4
yrachid <anuar.yasser@gmail.com>
2020-07-12 16:44:50 -0300

    Getting back with FizzBuzz
    Resuming Clojure studies with FizzBuzz. Getting to know how cond is
    useful in this situation and remembering that every single operator is a
    function and should, therefore, be used as such:
    
    (mod n 3) == 0 ; Compiles but does not work as expected
    (= 0 (mod n 3)) ; This is the correct form
    
    Also learned that cond does not require :else to be described, nil will
    be returned when no conditions are met.

---
+++ src/hello_clojure/fizz_buzz.clj
@@ -0,0 +1,15 @@
+(ns hello-clojure.fizz-buzz)
+
+(defn fizz
+  [n] (= (mod n 3) 0))
+
+(defn buzz
+  [n]
+  (= (mod n 5) 0))
+
+(defn fizz-buzz
+  [n]
+  (cond
+    (and (fizz n) (buzz n)) "FizzBuzz"
+    (fizz n) "Fizz"))
+
---
+++ test/hello_clojure/fizz_buzz_test.clj
@@ -0,0 +1,11 @@
+(ns hello-clojure.fizz-buzz-test
+  (:require [clojure.test :refer :all]
+            [hello-clojure.fizz-buzz :refer [fizz-buzz]]))
+
+(deftest when-number-is-both-fizz-buzz
+  (testing "Number is both Fizz and Buzz"
+    (is (= "FizzBuzz" (fizz-buzz 15)))))
+
+(deftest when-number-is-only-fizz
+  (testing "Number is only Fizz"
+    (is (= "Fizz" (fizz-buzz 3)))))
